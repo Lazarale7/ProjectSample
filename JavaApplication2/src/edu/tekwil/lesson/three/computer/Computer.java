cc95096b54fb517c54b3065094a58dbe1d46f9f4
Lazarale <lazarale@gmail.com>
2019-03-14 07:44:30 +0200

    1 lesson

---
+++ JavaApplication2/src/edu/tekwil/lesson/three/computer/Computer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.tekwil.lesson.three.computer;
+import edu.tekwil.lesson.three.computer.components.HardDrive;
+import edu.tekwil.lesson.three.computer.components.Motherboard;
+import edu.tekwil.lesson.three.computer.components.Processor;
+import edu.tekwil.lesson.three.computer.peripherals.*;
+
+/**
+ *
+ * @author user-b
+ */
+public class Computer {
+    HardDrive hdd;
+    Motherboard board;
+    Processor proc;
+    Keyboard key;
+    Monitor lcd;
+    Mouse point;
+    
+           
+}
