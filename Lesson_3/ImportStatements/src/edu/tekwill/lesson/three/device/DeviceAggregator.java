a940736f9f16d43766c7f9156e377bacd78a1367
Lazarale <lazarale@gmail.com>
2019-03-15 19:03:29 +0200

    lesson 3 corrected

--- Lesson_3/ImportStatements/src/edu/tekwill/lesson/three/device/DeviceAggregator.java
+++ Lesson_3/ImportStatements/src/edu/tekwill/lesson/three/device/DeviceAggregator.java
@@ -1,9 +1,11 @@
 package edu.tekwill.lesson.three.device;
 
-import edu.tekwill.lesson.three.device.data.Device;
-import edu.tekwill.lesson.three.device.model.*;
-public class DeviceAggregator {
 
+
+import edu.tekwill.lesson.three.device.data.Device;
+
+public class DeviceAggregator {
+edu.tekwill.lesson.three.device.model.Device modelDevice;
   Device dataDevice;
-  Device modelDevice;
+  
 }
