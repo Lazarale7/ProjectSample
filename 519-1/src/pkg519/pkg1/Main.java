8fbf1d75dc6e30a153b3e2156ac4fd057d2f2164
Alex <lazarale@gmail.com>
2019-03-29 12:13:35 +0200

    Lesson 5 (For, while)

---
+++ 519-1/src/pkg519/pkg1/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg519.pkg1;
+
+/**
+ *
+ * @author Alex
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0;
+        int sum = 0;
+        while (i<=100)
+        {
+            sum+=i;
+            i++;
+        }
+        System.out.println("Sum = " +sum);
+    }
+    
+}
